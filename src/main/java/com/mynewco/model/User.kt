23239563fd0af92fda2348bf1169fc6e815051c6
mauriziosodano <maurizio.sodano@gmail.com>
2022-03-21 19:37:42 +0100

    added db userDaoImpl saveMultiple test

--- src/main/java/com/mynewco/model/User.kt
+++ src/main/java/com/mynewco/model/User.kt
@@ -17,4 +17,23 @@ class User {
         return "User(id=$id, name='$name')"
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as User
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + name.hashCode()
+        return result
+    }
+
+
 }
--- src/test/java/com/mynewco/db/UserDaoImplTest.kt
+++ src/test/java/com/mynewco/db/UserDaoImplTest.kt
@@ -2,51 +2,140 @@ package com.mynewco.db
 
 import com.mynewco.model.User
 import org.junit.After
-import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
+import org.junit.Test
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.sql.Connection
-import java.util.Properties
+import java.util.function.Supplier
+import kotlin.streams.asSequence
+
 
 class UserDaoImplTest {
-    private  var conn: Connection? = null
+    private var conn: Connection? = null
+
+    private var users: List<User>? = null
+
+    companion object {
+        const val NUM_TEST_USERS = 2
+    }
+
+    private fun loadUsers(): List<User> {
+        //val users = arrayListOf<User>()
+        val path = Paths.get(
+            Companion::class.java.classLoader?.getResource("greaterexpectations.txt")?.toURI()!!
+        )
+
+        val streamSupplier =
+            Supplier {
+                Files.lines(path).map { line -> line.split("\\W+".toRegex()) }
+            }
+        //val temp = streamSupplier.get().asSequence().flatten().filter { word -> word.length > 3 }
+
+
+        //temp.forEach(::println)
+        return streamSupplier.get().asSequence()
+            .flatten()
+            .filter { word -> word.length > 3 }
+            .take(NUM_TEST_USERS)
+            .map { name -> User(name) }
+
+            .toList()
+
+
+    }
 
     @After
     fun tearDown() {
         println("After")
-        val db=Database.instance
+        val db = Database.instance
         db.close()
     }
 
     @Before
     fun setUp() {
         println("Before")
-        val db=Database.instance
-        val props=Profile.getProperties("db")
+        users = loadUsers()
+        println(users?.size)
+        //users?.forEach { user -> run { val userDao = UserDaoImpl(); userDao.save(user); } }
+        val db = Database.instance
+        val props = Profile.getProperties("db")
 
         db.connect(props)
-        conn=db.connection
-        conn?.autoCommit=false
+        conn = db.connection
+        conn?.autoCommit = false
     }
 
     @Test
     fun save() {
-        val user=User("Jupiter")
-        val userDao=UserDaoImpl()
+        val user = User("Jupiter")
+        val userDao = UserDaoImpl()
 
         userDao.save(user)
-        val stmt=conn?.createStatement()
-        val rs=stmt?.executeQuery("select id, name from user order by id desc;")
+        val stmt = conn?.createStatement()
+        val rs = stmt?.executeQuery("select id, name from user order by id desc;")
 
-        val result=rs?.next()==true
-        assertTrue("cannot retrieve inserted user",result)
+        val result = rs?.next() == true
+        assertTrue("cannot retrieve inserted user", result)
 
-        val name=rs?.getString("name")
+        val name = rs?.getString("name")
 
-        assertEquals("user name doesn't match retrieved",user.name,name)
+        assertEquals("user name doesn't match retrieved", user.name, name)
 
         stmt?.close()
+
+    }
+
+
+    private fun getMaxId(): Int {
+        val stmt = conn?.createStatement()
+        val rs = stmt?.executeQuery("select max(id) as id from user")
+
+        val maxInt = if (rs?.next() == true) {
+            rs.getInt("id")
+
+        } else {
+            0
+        }
+
+        stmt?.close()
+        return maxInt
+    }
+
+    private fun getUserInRange(minId: Int, maxId: Int): List<User> {
+        val stmt = conn?.prepareStatement("select id, name from user where id>=? and id<=?")
+        stmt?.setInt(1, minId)
+        stmt?.setInt(2, maxId)
+        val rs=stmt?.executeQuery()
+        val users= arrayListOf<User>()
+        while (rs?.next()!=false){
+            val id= rs!!.getInt("id")
+            val name= rs.getString("name")
+            users.add(User(id,name))
+        }
+        stmt.close()
+        return users
+    }
+
+    @Test
+    fun saveMultiple() {
+        val userDao = UserDaoImpl()
+        for (user in users!!) {
+            userDao.save(user)
+        }
+        val maxId = getMaxId()
+
+        println("maxId=$maxId")
+        for ( i  in 0 until users!!.size){
+            val id=(maxId-users!!.size)+i+1
+            println("id=$id")
+            users!![i].id=id
+        }
+        val retrievedUsers=getUserInRange(maxId-users!!.size+1,maxId)
+        assertEquals("max Id differs from expected", retrievedUsers.size, NUM_TEST_USERS)
+        assertEquals("max Id differs from expected", users,retrievedUsers)
 
     }
 }
